b760ec52a60a5ff2c4c8e65e503d0e72913662ae
Derick2328 <derickxavier23.xr@hotmail.com>
2025-06-27 11:07:41 -0700

    Update bedrockBot.js

--- bedrockBot.js
+++ bedrockBot.js
@@ -51,7 +51,8 @@ const server = http.createServer((req, res) => {
 
 const PORT = process.env.PORT || 3000;
 server.listen(PORT, () => {
-  console.log(Servidor HTTP activo en puerto ${PORT});
+console.log(`Servidor HTTP activo en puerto ${PORT}`);
+
 });
 
 // Iniciar bot
